671027a49e6b5d664a47fc378b2d13abe61e524a
kohei morita <k_morita_gr@yahoo.co.jp>
2013-05-03 00:05:24 +0900

    21

--- Problem21.py
+++ Problem21.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
-from datetime import date, timedelta
-from calendar import monthrange
+
+def divsum(n):
+    k = 0
+    for i in range(1, n):
+        if (n % i) == 0:
+            k += i
+    return k
 
 if __name__ == "__main__":
-    k = 1
-    for i in range(1, 100):
-        k *= i
-    s = str(k)
     k = 0
-    for i in map(int, s):
-        k += i
+    for i in range(1, 10000):
+        j = divsum(i)
+        if i != j and i == divsum(j):
+            k += i
+            print(i)
     print(k)
